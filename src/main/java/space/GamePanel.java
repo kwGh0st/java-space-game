d3ba9fc90b8b7579a90d25ce4c248961f4604dd2
Karol Programuje <karol341654@gmail.com>
2023-10-13 20:57:57 +0200

    GamePanel code changes.

--- src/main/java/space/GamePanel.java
+++ src/main/java/space/GamePanel.java
@@ -2,14 +2,83 @@ package main.java.space;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class GamePanel extends JPanel {
-    private static final int GAME_WIDTH = 1000;
-    private static final int GAME_HEIGHT = 555;
-    private static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+public class GamePanel extends JPanel implements Runnable {
+    private static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    static final int GAME_WIDTH = (int) (SCREEN_SIZE.getWidth() * 0.50);
+    private static final int GAME_HEIGHT = (int) (SCREEN_SIZE.getHeight() * 0.75);
+    private List<Ship> enemies;
+    private final GamerShip gamer;
+    private static final Random random = new Random();
 
     public GamePanel() {
-        this.setPreferredSize(SCREEN_SIZE);
+        createEnemies();
+        gamer = new GamerShip(new ImageIcon("C:\\Users\\Karol\\Desktop\\Projekty na Github\\Space\\src\\img\\ship.png").getImage());
+        this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
         this.setBackground(Color.black);
+        this.addKeyListener(new AL(gamer));
+        this.setFocusable(true);
+        Thread gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void createEnemies() {
+        enemies = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            enemies.add(new Ship(new ImageIcon("C:\\Users\\Karol\\Desktop\\Projekty na Github\\Space\\src\\img\\enemy.png").getImage(), this));
+        }
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        enemies.forEach(e -> g2d.drawImage(e.getShipImage(), e.getX(), e.getY(), this));
+        g2d.drawImage(gamer.getShipImage(), gamer.getX(), GAME_HEIGHT - gamer.getShipImage().getHeight(this), this);
+    }
+
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while (true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                enemies.forEach(Ship::move);
+                gamer.move();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private class AL extends KeyAdapter {
+        private final GamerShip ship;
+
+        public AL(GamerShip ship) {
+            this.ship = ship;
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            ship.keyPressed(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            ship.keyReleased(e);
+        }
     }
 }
