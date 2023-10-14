2b885410816f6005a23b5a1a7a622c150380c2fe
Karol Programuje <karol341654@gmail.com>
2023-10-14 13:15:14 +0200

    Adding Rocket

--- src/main/java/space/Rocket.java
+++ src/main/java/space/Rocket.java
@@ -1,6 +1,26 @@
 package main.java.space;
 
-public class Rocket {
-    private int yVelocity;
+import java.awt.*;
 
+public class Rocket extends Rectangle {
+    private final int speed;
+
+    public Rocket(int x, int y, int width, int height, int speed) {
+        super(x, y, width, height);
+        this.speed = speed;
+
+    }
+
+    public void move() {
+        y -= speed;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(x, y, width, height);
+    }
+
+    public boolean isOutOfScreen() {
+        return y < 0;
+    }
 }
