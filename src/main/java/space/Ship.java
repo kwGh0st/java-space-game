5a72e0e8a0df2a45c6ea1c9f9bb4d4eab78552e3
Karol Programuje <karol341654@gmail.com>
2023-10-14 20:55:00 +0200

    Code changes.

--- src/main/java/space/Ship.java
+++ src/main/java/space/Ship.java
@@ -2,28 +2,46 @@ package main.java.space;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Ship {
+public class Ship implements ActionListener {
     private final Image shipImage;
     protected int xVelocity;
     private int yVelocity;
+    private int rocketSpeed;
     protected int x;
     protected int y;
-    private JPanel panel;
     private static final Random random = new Random();
+    protected List<Rocket> rockets;
+    private Timer timer;
 
     public Ship(Image shipImage) {
         this.shipImage = shipImage;
+        rockets = new ArrayList<>();
     }
 
-    public Ship(Image shipImage, JPanel panel) {
+    public Ship(Image shipImage, int rocketSpeed) {
         this.shipImage = shipImage;
-        this.panel = panel;
-        this.xVelocity = random.nextInt(5) + 1;
-        this.yVelocity = random.nextInt(5) + 1;
-        this.x = random.nextInt(GamePanel.GAME_WIDTH - shipImage.getWidth(null));
+        rockets = new ArrayList<>();
+        this.timer = new Timer(3000, this);
+        this.timer.start();
+        this.rocketSpeed = rocketSpeed;
+    }
 
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public void shootRandomly() {
+        int r = random.nextInt(10);
+        if (r == 5 || r == 1 || r == 3 || r == 2) {
+            rockets.add(new Rocket(getX() + (getShipImage().getWidth(null) / 2),
+                    getY() + getShipImage().getHeight(null), 10, 10, Math.negateExact(rocketSpeed), Color.red));
+        }
     }
 
     public void move() {
@@ -32,7 +50,7 @@ public class Ship {
     }
 
     private void checkBorders() {
-        if (x >= (panel.getWidth() - this.getShipImage().getWidth(null)) || x < 0) {
+        if (x >= (GamePanel.GAME_WIDTH - this.getShipImage().getWidth(null)) || x < 0) {
             xVelocity = -xVelocity;
         }
     }
@@ -41,8 +59,13 @@ public class Ship {
         return shipImage;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setX() {
+        this.x = random.nextInt(GamePanel.GAME_WIDTH - shipImage.getWidth(null));
+    }
+
+    public void setXVelocity() {
+        this.xVelocity = random.nextInt(5) + 1;
+        this.yVelocity = random.nextInt(5) + 1;
     }
 
     public int getX() {
@@ -53,4 +76,8 @@ public class Ship {
         return y;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        shootRandomly();
+    }
 }
