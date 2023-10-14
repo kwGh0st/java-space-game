88530eb51896dd46feb11d281f292b3ecfbbe0b8
Karol Programuje <karol341654@gmail.com>
2023-10-14 20:54:49 +0200

    Code changes.

--- src/main/java/space/Rocket.java
+++ src/main/java/space/Rocket.java
@@ -3,11 +3,13 @@ package main.java.space;
 import java.awt.*;
 
 public class Rocket extends Rectangle {
-    private final int speed;
+    private int speed;
+    private Color color;
 
-    public Rocket(int x, int y, int width, int height, int speed) {
+    public Rocket(int x, int y, int width, int height, int speed, Color color) {
         super(x, y, width, height);
         this.speed = speed;
+        this.color = color;
 
     }
 
@@ -15,12 +17,13 @@ public class Rocket extends Rectangle {
         y -= speed;
     }
 
+
     public void draw(Graphics g) {
-        g.setColor(Color.red);
+        g.setColor(color);
         g.fillOval(x, y, width, height);
     }
 
     public boolean isOutOfScreen() {
-        return y < 0;
+        return y < 0 || y > GamePanel.GAME_HEIGHT;
     }
 }
