b22599a1ba2cc7bed485bad37ea305d946be420c
Karol Programuje <karol341654@gmail.com>
2023-10-20 20:19:58 +0200

    Code changes.

--- src/main/java/space/GameFrame.java
+++ src/main/java/space/GameFrame.java
@@ -3,10 +3,9 @@ package main.java.space;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-    private GamePanel panel;
 
     public GameFrame() {
-        panel = new GamePanel();
+        GamePanel panel = new GamePanel();
         this.add(panel);
         this.setResizable(false);
         this.setTitle("Space Game");
